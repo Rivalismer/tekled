0cb027eb401a5787bcf923f8909b98eeb5b78009
Ask <ask.e.oren@gmail.com>
2021-12-16 14:20:00 +0100

    Start of project

---
+++ NNV/nnv.c
@@ -0,0 +1,7 @@
+#include "nnv.h"
+
+static float temp_r = 0.5;
+
+float nnv_finite(float* k_arr, float g, int n_e, float r){
+
+}
---
+++ NNV/nnv.h
@@ -0,0 +1,12 @@
+#ifndef __NNV_H__
+#define __NNV_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+float nnv_finite(float* k_arr, float g, int n_e, float r);
+
+float nnv_ir_finite()
+
+#endif
